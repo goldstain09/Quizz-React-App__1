3b6e5a675306c5ef95cd43efe642196b3c7706a1
Sujal Rajput <rajputsujal992@gmail.com>
2023-06-26 17:04:55 +0530

    Quizz React App

---
+++ src/Components/QuizNormal.jsx
@@ -0,0 +1,155 @@
+import Store from '../Store'
+import './style.css'
+import React, { useContext,useState,useEffect, useReducer } from 'react'
+import QuizHard from './QuizHard'
+import QuizReducer from '../Reducer'
+
+
+export default function QuizNormal() {  
+
+  const [markN, setMarkN] = useState(0)
+
+  const [checkE, setCheckE] = useState(false)
+
+  const [selectedOption, setSelectedOption] = useState('')
+  
+  const [btn_click, setBtn_Click] = useState(0)
+
+
+  const [totalMarkNs, dispatch] =useReducer(QuizReducer, 0)
+// console.log(totalMarkNs);
+
+
+
+  useEffect(() => { 
+    if(btn_click===10){
+        document.getElementById('QuizNormalResult_btn').style.display='inline-block';
+    }   
+  },
+  [btn_click===10]
+  )
+
+  useEffect(() => {
+    if(checkE){
+        dispatch({
+            type:'Normal',
+            payload:markN
+        });
+        document.getElementById('QuizNormalResult_btn').style.display='none';     
+    }
+  },[checkE])
+
+ 
+ 
+
+  
+  const c = useContext(Store)
+  return (
+    <>
+
+      <h2 align='center'>Normal Level</h2>
+
+     {
+                c.QuizNormal.length > 0 ? c.QuizNormal.map((item, index) => (
+                    <div key={item.id}>
+                        <h3 className='text-center mt-4' >Ques.{index + 1}){item.question}</h3>
+                        <div className='text-center mx-1'>
+                            <p className='form-check-label' >(a)&nbsp;{item.opt1}</p>
+                            <input 
+                                required
+                                name={index}
+                                type='radio'
+                                value={item.opt1}
+                                className={`form-check-input   ${item.id} for_result_btn`}
+                                checked={selectedOption === item.opt1}
+                                onChange={(event) => {
+                                    setCheckE(false)
+                                    setSelectedOption(event.target.value)
+                                document.getElementById(item.id).style.display='inline-block';
+                                }} />
+                        </div>
+                        <div className='text-center mx-1'>
+                            <p className='form-check-label'>(b)&nbsp;{item.opt2}</p>
+                            <input
+                                required
+                                name={index}
+                                type='radio'
+                                value={item.opt2}
+                                className={`form-check-input  ${item.id}  for_result_btn`}
+                                checked={selectedOption === item.opt2}
+                                onChange={(event) => {
+                                    setCheckE(false)
+                                    setSelectedOption(event.target.value)
+                                    document.getElementById(item.id).style.display='inline-block';
+                                }} />
+                        </div>
+                        <div className='text-center mx-1'>
+                            <p className='form-check-label' >(c)&nbsp;{item.opt3}</p>
+                            <input
+                                required
+                                name={index}
+                                type='radio'
+                                value={item.opt3}
+                                className={`form-check-input  ${item.id} for_result_btn`}
+                                checked={selectedOption === item.opt3}
+                                onChange={(event) => {
+                                    setCheckE(false)
+                                    setSelectedOption(event.target.value)
+                                    document.getElementById(item.id).style.display='inline-block';
+                                }} />
+                        </div>
+                        <div className='text-center mx-1'>
+                            <p className='form-check-label'>(d)&nbsp;{item.opt4}</p>
+                            <input
+                                required
+                                name={index}
+                                type='radio'
+                                value={item.opt4}
+                                className={`form-check-input  ${item.id} for_result_btn`}
+                                checked={selectedOption === item.opt4}
+                                onChange={(event) => {
+                                    setCheckE(false)
+                                    setSelectedOption(event.target.value);
+                                    document.getElementById(item.id).style.display='inline-block';
+                                }} />
+
+                        </div>
+                        <div>
+                            <button id={item.id} className={`btn btn-outline-success submitbtn ${item.id}`} onClick={() => {
+                                if (item.rightAnswer === selectedOption) {
+                                    setMarkN(markN + 1);
+                                }
+                                // its for disabbling a quiz which is submmitted 
+                                let elem = document.getElementsByClassName(item.id)
+                                for (let i = 0; i < elem.length; i++) {
+                                     elem[i].setAttribute('disabled','')
+                                }
+                                setBtn_Click(btn_click+1)
+                                
+                            }} type='submit' style={{display:'none'}} ><i class="bi bi-check2-circle"></i></button>
+                        </div>
+                    </div>
+                )) : <p></p>
+            }
+
+            <div className='text-center py-4'>
+                <button 
+                id='QuizNormalResult_btn' 
+                style={{display:'none'}} 
+                className=' btn btn-outline-warning text-center'
+                onClick={()=>{
+                    setCheckE(true)
+                }} 
+                >Submit & <br/> Move on to Hard Level
+                </button>
+            </div>
+
+            <div className='text-center'>
+               {
+                checkE && <QuizHard/>
+               }
+            </div>
+
+    </>
+  )
+}
---
+++ src/Components/Result.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useReducer } from 'react'
+import QuizReducer from '../Reducer'
+
+export default function Result({ checkH }) {
+
+  const [allValues, dispatch] = useReducer(QuizReducer, 0)
+  const { easy, normal, hard, name } = allValues
+  //  console.log(easy,normal,hard);
+  useEffect(() => {
+    dispatch({ type: 'separateMarks' })
+  }, [checkH])
+
+  return (
+    <>
+      <div align='center'>
+        <h5>
+          {
+            `Hello ${name}, You got`
+          }
+        </h5>
+      </div>
+      <button className='btn btn-outline-warning mx-2' disabled>{`${easy}/10 in Easy`}</button>
+      <button className='btn btn-outline-warning mx-2' disabled>{`${normal}/10 in Normal`}</button>
+      <button className='btn btn-outline-warning mx-2' disabled>{`${hard}/10 in Hard`}</button>
+      <br />
+      <button className='text-center btn btn-dark mt-4' style={{ fontSize: '3rem' }} disabled >
+        {`${name}, your TOTAL marks - ${easy + normal + hard}/30`}
+      </button>
+    </>
+  )
+}
